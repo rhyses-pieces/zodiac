64b4d4d8e85d80c81d63794290e232ec90c3f4a2
Kendra Walton <mswalton15@gmail.com>
2020-09-12 18:06:02 -0400

    connect back to front

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { ZodiacHomeComponent } from './zodiac-home/zodiac-home.component'
+import { ZodiacHomeComponent } from './zodiac-home/zodiac-home.component';
+import { FollowingComponent} from './following/following.component';
 
 const routes: Routes = [
   {
@@ -26,6 +27,10 @@ const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent
+  },
+  {
+    path: 'user',
+    component: FollowingComponent
   }
 ];
 
